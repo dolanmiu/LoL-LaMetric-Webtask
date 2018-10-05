091d9ea098bb4b920cb464446d228b1eb34a84c6
Dolan <dolan_miu@hotmail.com>
2018-10-05 02:03:54 +0100

    Add test for new positions endpoint

--- e2e/endpoint.spec.js
+++ e2e/endpoint.spec.js
@@ -1,7 +1,11 @@
-const { expect } = require("chai");
+const chai = require("chai");
 const request = require("request-promise");
 
 const apiKey = process.env.RIOT_API_KEY;
+
+const { expect } = chai;
+chai.should();
+chai.use(require("chai-things"));
 
 async function getEndPoint(url) {
     return await request({
@@ -48,6 +52,15 @@ async function test() {
     expect(response)
         .to.have.property("services")
         .that.is.an("array").which.is.not.empty;
+
+    response = await getEndPoint(`https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/27692762?api_key=${apiKey}`);
+    response.should.all.have.property("queueType");
+    response.should.all.have.property("hotStreak");
+    response.should.all.have.property("wins");
+    response.should.all.have.property("losses");
+    response.should.all.have.property("rank");
+    response.should.all.have.property("tier");
+    response.should.all.have.property("leaguePdoints");
 }
 
 test();
